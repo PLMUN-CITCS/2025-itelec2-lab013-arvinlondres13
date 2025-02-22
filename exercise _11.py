89324fa5db972d2522dc30a5d3b84a537c1c9b88
arvinlondres13 <arvinlondres_bsit@plmun.edu.ph>
2025-02-23 07:49:30 +0800

    exercise _11.py

--- exercise _11.py
+++ exercise _11.py
@@ -1,7 +1,6 @@
 numbers = list(range(1, 11))  # Create a list from 1 to 10
 
 for num in numbers:
-  
     if num == 3:
         continue  # Skip the rest of this iteration when num is 3
 
@@ -9,3 +8,4 @@ for num in numbers:
         break  # Exit the loop completely when num is 7
 
     print(num)  # Print the number if it's not skipped or breaking the loop
+
